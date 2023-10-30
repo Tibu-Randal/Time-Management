c5268e41f92394e9017e3e6bbffe361c76e2c3aa
Tibu-Randal <tiburandal@gmail.com>
2023-10-30 21:22:05 +0530

    Completed program

--- quickstart.py
+++ quickstart.py
@@ -4,6 +4,8 @@ import datetime
 import os.path
 import sqlite3
 
+from sys import argv
+
 from dateutil import parser
 
 from google.auth.transport.requests import Request
@@ -12,21 +14,15 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-# If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 
 def main():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
+
     creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
+
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -37,16 +33,21 @@ def main():
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
-    commitHours(creds)
+    if argv[1] == 'add':
+        duration = argv[2]
+        description = argv[3]
+        addEvent(creds, duration, description)
+    if argv[1] == 'commit':
+        commitHours(creds)
+
 
 def commitHours(creds):
     try:
         service = build('calendar', 'v3', credentials=creds)
 
-        # Call the Calendar API
         today= datetime.date.today()
         timeStart = str(today) + "T00:00:00Z"
-        timeEnd = str(today) + "T23:59:59Z" # 'Z' indicates UTC time
+        timeEnd = str(today) + "T23:59:59Z" 
         print("Getting today's coding hours")
         events_result = service.events().list(calendarId='fdfd294a77a2d8bfede4afb49a46eaf4e68905cd6a79872674232f0428549fc8@group.calendar.google.com', timeMin=timeStart, timeMax=timeEnd, singleEvents=True, orderBy='startTime', timeZone='Asia/Kolkata').execute()
         events = events_result.get('items', [])
@@ -65,8 +66,8 @@ def commitHours(creds):
             start = event['start'].get('dateTime', event['start'].get('date'))
             end = event['end'].get('dateTime', event['end'].get('date'))
 
-            start_formatted = parser.isoparse(start) # changing the start time to datetime format
-            end_formatted = parser.isoparse(end) # changing the end time to datetime format
+            start_formatted = parser.isoparse(start) 
+            end_formatted = parser.isoparse(end) 
             duration = end_formatted - start_formatted
 
             total_duration += duration
@@ -82,11 +83,33 @@ def commitHours(creds):
         coding_hours = (date, 'CODING', formatted_total_duration)
         cur.execute("INSERT INTO hours VALUES(?, ?, ?);", coding_hours)
         conn.commit()
-
+        print("Coding hours added to Database")
 
     except HttpError as error:
         print('An error occurred: %s' % error)
 
+def addEvent(creds, duration, description):
+    start = datetime.datetime.utcnow()
+    
+    end = datetime.datetime.utcnow() + datetime.timedelta(hours=int(duration))
+    start_formatted = start.isoformat() + 'Z'
+    end_formatted = end.isoformat() + 'Z'
+
+    event = {
+    'summary': description,
+    'start': {
+        'dateTime': start_formatted,
+        'timeZone': 'Asia/Kolkata',
+        },
+    'end': {
+        'dateTime': end_formatted,
+        'timeZone': 'Asia/Kolkata',
+        },
+    }
+
+    service = build('calendar', 'v3', credentials=creds)
+    event = service.events().insert(calendarId='fdfd294a77a2d8bfede4afb49a46eaf4e68905cd6a79872674232f0428549fc8@group.calendar.google.com', body=event).execute()
+    print('Event created: %s' % (event.get('htmlLink')))
 
 if __name__ == '__main__':
     main()
